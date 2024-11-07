46926b7589eb62804de4e11277f021350f0acf14
bo <cbs1006@naver.com>
2024-11-07 10:22:36 +0900

    Feat: redis host, port 추가

--- src/main/java/shop/s5g/auth/config/RedisConfig.java
+++ src/main/java/shop/s5g/auth/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package shop.s5g.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,10 +10,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(host, port);
     }
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
